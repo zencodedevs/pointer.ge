0e55cf46331e20fe7b22d608c934c8a5075b6f94
gulisa.merebashvili <gulisa.merebashvili@gmail.com>
2021-10-18 10:57:24 +0400

    text correctioin

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,20 +1,18 @@
-'use strict';
+"use strict";
 
 //Fixed-top menu
 function fixedHeader() {
   let ww = $(window).scrollTop();
   if (ww > 0) {
-    $('.menu').addClass('menu--active');
-    $('.mobile-menu').addClass('mobile-menu--scroll');
+    $(".menu").addClass("menu--active");
+    $(".mobile-menu").addClass("mobile-menu--scroll");
   } else {
-    $('.menu').removeClass('menu--active');
-    $('.mobile-menu').removeClass('mobile-menu--scroll');
+    $(".menu").removeClass("menu--active");
+    $(".mobile-menu").removeClass("mobile-menu--scroll");
   }
 }
 
 fixedHeader();
-$(window).on('scroll', function () {
+$(window).on("scroll", function () {
   fixedHeader();
 });
-
-
